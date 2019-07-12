a2cf36e442ace479fb901c5ab8c0a10727caa58a
mirandov <tremors224@gmail.com>
2019-07-12 12:30:07 +0300

    master: fix 5

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,6 @@ ActiveRecord::Schema.define(version: 20190712064419) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
-    t.string   "value"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "excess_definitions", force: :cascade do |t|
     t.string   "v_value"
     t.string   "d_value"
