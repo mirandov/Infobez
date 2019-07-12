cc65bd76f041aee29fa60471012a310bd5266b74
mirandov <tremors224@gmail.com>
2019-07-12 10:52:13 +0300

    master: new versions

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem "rails_12factor", group: :production
-gem 'rails', '4.2.8'
+gem 'rails', '5.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'pg', '~> 0.18.0'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,15 @@ ActiveRecord::Schema.define(version: 20190712064419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
   create_table "excess_definitions", force: :cascade do |t|
     t.string   "v_value"
     t.string   "d_value"
@@ -49,15 +57,7 @@ ActiveRecord::Schema.define(version: 20190712064419) do
     t.datetime "updated_at",        null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-  end
+
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
