d1b23f756a08799f7a7e9d3a9e7afb13716e21a9
mirandov <tremors224@gmail.com>
2019-11-19 16:30:37 +0300

    master: some fix 19.11

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,6 @@
 class Admin < ActiveRecord::Base
   has_many :check_lists
+  validates :first_name, :second_name, :patronymic, presence: true
 
   def full_name
     "#{first_name} #{second_name} #{patronymic}"
