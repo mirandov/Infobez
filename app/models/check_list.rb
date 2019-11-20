4d339eedc0415a48e70fe4af4b7dc3d1aca305b9
mirandov <tremors224@gmail.com>
2019-11-20 13:33:13 +0300

    master some fix 2011

--- app/models/check_list.rb
+++ app/models/check_list.rb
@@ -6,4 +6,6 @@ class CheckList < ActiveRecord::Base
 
   has_many :orders
   accepts_nested_attributes_for :orders, reject_if: :all_blank, allow_destroy: true
+
+  validates :admin_id, :order_date, :sale, :comment, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ActiveRecord::Base
   belongs_to :service
   belongs_to :person
   belongs_to :check_list
+
+  validates :price, :name_service, presence: true
 end
